bbab709f23a393fd7d045ecbd1221d21edee45fa
Miguel Antunez <miguel.antunez@upcnet.es>
2021-04-25 00:04:46 +0200

    update reproductions

--- src/main/java/isdcm/restserver/controller/VideoController.java
+++ src/main/java/isdcm/restserver/controller/VideoController.java
@@ -4,8 +4,8 @@ import isdcm.restserver.dto.VideoDTO;
 import isdcm.restserver.service.VideoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -18,8 +18,8 @@ public class VideoController {
 
     public VideoController(VideoService videoService) {this.videoService = videoService;}
 
-    @PutMapping()
-    public ResponseEntity<VideoDTO> updateReproductions(@RequestBody Integer id) {
+    @PutMapping("/{id}")
+    public ResponseEntity<VideoDTO> updateReproductions(@PathVariable("id") Integer id) {
         Optional<VideoDTO> optionalVideoDTO = videoService.updateVideo(id);
         return optionalVideoDTO.map(dto -> ResponseEntity.ok().body(dto)).orElseGet(() -> ResponseEntity.notFound().build());
     }
